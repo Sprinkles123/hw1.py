796acf056febe021fa8dc318ad52da4ef1fcf1df
Sprinkles123 <42747869+Sprinkles123@users.noreply.github.com>
2019-01-28 22:20:40 -0500

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -18,7 +18,7 @@ def isDivBySeven(x):
 # output: True or False
 def isPrime(x):
 	print(x)
-    for i in range(2, int(x ** 0.5) + 1) :
+    for i in range(2, int(x ** 0.5) + 1):
         if x % i == 0:
             return False
     return True
